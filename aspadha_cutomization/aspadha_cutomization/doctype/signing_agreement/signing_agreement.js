8a79efa82dbc18613e7450acb8033b029c2ae2c4
kushika-rspl <kushika@rajlaxmiworld.com>
2025-03-12 09:39:31 +0000

    Print format and o=doctypes

--- aspadha_cutomization/aspadha_cutomization/doctype/signing_agreement/signing_agreement.js
+++ aspadha_cutomization/aspadha_cutomization/doctype/signing_agreement/signing_agreement.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, kushika and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Signing Terms and Conditions", {
+// frappe.ui.form.on("Signing Agreement", {
 // 	refresh(frm) {
 
 // 	},
--- aspadha_cutomization/aspadha_cutomization/doctype/signing_agreement/signing_agreement.py
+++ aspadha_cutomization/aspadha_cutomization/doctype/signing_agreement/signing_agreement.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class SigningTermsandConditions(Document):
+class SigningAgreement(Document):
 	pass
